52b707de8d7623b5ceee95d7eb6b8b609c938c92
nandugokulkn <nandugokul@probeplus.in>
2024-05-16 20:51:49 +0530

    zooming done browser zooming needs to be done

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -1,5 +1,29 @@
+import { useState } from "react";
+
 const HomePage = () => {
-  return <div className="bg-white w-full h-screen">this is the new thing</div>;
+  const [scale, setScale] = useState(1);
+  const handleWheel = (event: any) => {
+    if (event.ctrlKey) {
+      let zoomDirection = 1;
+      if (event.deltaY > 0) {
+        zoomDirection = -1;
+      }
+      setScale((prevScale) => prevScale + zoomDirection * 0.1);
+    }
+  };
+
+  return (
+    <div
+      className="bg-white w-full h-screen"
+      style={{
+        transform: `scale(${scale})`,
+        transition: "transform 0.3s ease",
+      }}
+      onWheel={handleWheel}
+    >
+      This is the new thing
+    </div>
+  );
 };
 
 export default HomePage;
